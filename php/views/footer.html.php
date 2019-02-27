c174800b57b2b8b0d577c09b87cb95212629e684
loljoho <loljoho@users.noreply.github.com>
2019-02-26 22:21:52 -0500

    Update jQuery link

--- php/views/footer.html.php
+++ php/views/footer.html.php
@@ -4,7 +4,7 @@
 
   </div> <!-- /container -->
 
-  <script src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
+  <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
   <script src="<?php echo $container->config['baseFolder']; ?>/js/bootstrap.min.js"></script>
   <script src="<?php echo $container->config['baseFolder']; ?>/js/triviatime.js"></script>
 
